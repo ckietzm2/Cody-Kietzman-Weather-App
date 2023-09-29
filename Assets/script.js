5e99b0ea25adef5963e08b2559fd5fbd1ea7c8b5
Cody K <ckietzm2@gmail.com>
2023-09-29 14:34:04 -0500

    Fetched API's and console logged data to show based on city input

--- Assets/script.js
+++ Assets/script.js
@@ -1,13 +1,27 @@
 var weatherURL = "https://api.openweathermap.org"
 var apiKey = "2deeb2a69137fff43aae291e7205b285"
 var weatherAPI = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=2deeb2a69137fff43aae291e7205b285"
+var city = ""
+
+$("#search").on("click",function(event) {
+  event.preventDefault()
+  city = $("#city-name").val()
+    if (city === "") {
+      alert("Please enter valid city")
+    }
+    else if (city != "") {
+      
+      getWeather()
+    }
+
+
+})
 
 
 function getWeather() {
-// var city = connect with input box from HTML (Use .val at end to grab)
 
-// var requestURL = "http://api.openweathermap.org/data/2.5/forecast?appid=c04c273159790588c5d89056e8655cce&units=imperial&q=${city}" 
-var requestURL = "http://api.openweathermap.org/data/2.5/forecast?appid=2deeb2a69137fff43aae291e7205b285&units=imperial&q=chicago"
+
+var requestURL = "http://api.openweathermap.org/data/2.5/forecast?appid=c04c273159790588c5d89056e8655cce&units=imperial&q="+city
 
 
 fetch(requestURL)
@@ -15,8 +29,39 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (data) {
-    console.log(data);
+    
+    var lat = data.city.coord.lat
+    var lon = data.city.coord.lon
+    
+    console.log(data)
+    
+    weatherAPI = "https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid=2deeb2a69137fff43aae291e7205b285"
+    fiveDayForecast()
   });
 }
 
-getWeather()
+function fiveDayForecast() {
+  fetch(weatherAPI)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+   
+  var temp = data.list[0].main.temp
+  var tempFar = (((temp-273.15)*9)/5)+32
+    tempFar = tempFar.toFixed(0)
+  var weatherDescription = data.list[0].weather[0].description
+  var windSpeed = data.list[0].wind.speed
+  var humidity = data.list[0].main.humidity
+    windSpeed = windSpeed.toFixed(0)
+    console.log(data)
+    console.log(tempFar)
+    console.log(weatherDescription)
+    console.log(windSpeed)
+    console.log(humidity)
+   
+  });
+}
+
+
+// 'http://openweathermap.org/img/w/${icon}.png'
