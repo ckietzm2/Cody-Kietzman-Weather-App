5e37723c8ae8f1b25d3f77501c36ed7c770022f0
Cody K <ckietzm2@gmail.com>
2023-09-30 12:41:31 -0500

    Current weather works and pulls data from weather API

--- Assets/script.js
+++ Assets/script.js
@@ -2,10 +2,20 @@ var weatherURL = "https://api.openweathermap.org"
 var apiKey = "2deeb2a69137fff43aae291e7205b285"
 var weatherAPI = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=2deeb2a69137fff43aae291e7205b285"
 var city = ""
+var weatherDetails = $("#weather-details")
+var imgURL = ""
+var imgWeather = $("#icon")
+var cityFirstLetter = ""
 
 $("#search").on("click",function(event) {
   event.preventDefault()
+  
   city = $("#city-name").val()
+
+cityFirstLetter=city.charAt(0).toUpperCase()
+city=cityFirstLetter+city.slice(1)
+
+
     if (city === "") {
       alert("Please enter valid city")
     }
@@ -16,6 +26,9 @@ $("#search").on("click",function(event) {
 
 
 })
+
+
+
 
 
 function getWeather() {
@@ -33,8 +46,6 @@ fetch(requestURL)
     var lat = data.city.coord.lat
     var lon = data.city.coord.lon
     
-    console.log(data)
-    
     weatherAPI = "https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid=2deeb2a69137fff43aae291e7205b285"
     fiveDayForecast()
   });
@@ -47,18 +58,26 @@ function fiveDayForecast() {
   })
   .then(function (data) {
    
-  var temp = data.list[0].main.temp
-  var tempFar = (((temp-273.15)*9)/5)+32
-    tempFar = tempFar.toFixed(0)
+  temperature = data.list[0].main.temp
+  temperature = (((temperature-273.15)*9)/5)+32
+    temperature = temperature.toFixed(1)
   var weatherDescription = data.list[0].weather[0].description
   var windSpeed = data.list[0].wind.speed
+    windSpeed = windSpeed.toFixed(1)
   var humidity = data.list[0].main.humidity
-    windSpeed = windSpeed.toFixed(0)
-    console.log(data)
-    console.log(tempFar)
-    console.log(weatherDescription)
-    console.log(windSpeed)
-    console.log(humidity)
+  var imgIcon = data.list[0].weather[0].icon
+      imgURL = 'http://openweathermap.org/img/w/'+imgIcon+'.png'
+    imgWeather.attr('src',imgURL)
+    weatherDetails.removeAttr('class')
+    
+    $("#current-weather").text("Current Weather in " + city)
+    $("#temp").text("Temperature:" + temperature + " degrees")
+    $("#speed").text("Wind Speed:" + windSpeed + " mph")
+    $("#description").text("Description:" + weatherDescription)
+    $("#humidity").text("Humidity:" + humidity + "%")
+    
+
+  
    
   });
 }
