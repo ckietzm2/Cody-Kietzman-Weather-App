05ced2e702b31acc316ac9040eb379ffd66cbbb9
Cody K <ckietzm2@gmail.com>
2023-10-01 16:28:04 -0500

    5 day forecast works.  Just need to finish local storage

--- Assets/script.js
+++ Assets/script.js
@@ -10,6 +10,12 @@ var imgURL1 = ""
 var imgWeather1 = $("#icon1")
 var imgURL2 = ""
 var imgWeather2 = $("#icon2")
+var imgURL3 = ""
+var imgWeather3 = $("#icon3")
+var imgURL4 = ""
+var imgWeather4 = $("#icon4")
+var imgURL5 = ""
+var imgWeather5 = $("#icon5")
 
 
 var cityFirstLetter = ""
@@ -33,6 +39,11 @@ var dayFour = dayjs().add(4, 'day').format('MMM D, YYYY');
 
 var timerPage5 = $('#day-five')
 var dayFive = dayjs().add(5, 'day').format('MMM D, YYYY');
+
+var currentYear = dayjs().format('YYYY')
+var currentMonth = dayjs().format('MM')
+var currentDTE = dayjs().add(5, 'hour').format('DD')
+var currentDate = currentYear+"-"+currentMonth+"-"+currentDTE+" 09:00:00"
 
 
 
@@ -74,24 +85,20 @@ fetch(requestURL)
   })
   .then(function (data) {
     
-    var lat = data.city.coord.lat
-    var lon = data.city.coord.lon
+    // var lat = data.city.coord.lat
+    // var lon = data.city.coord.lon
     
-    weatherAPI = "https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid=2deeb2a69137fff43aae291e7205b285"
-    currentForecast()
-  });
-}
+    // weatherAPI = "https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid=2deeb2a69137fff43aae291e7205b285"
 
-function currentForecast() {
-  fetch(weatherAPI)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
+  // fetch(weatherAPI)
+  // .then(function (response) {
+    // return response.json();
+  // })
+  // .then(function (data) {
    
 
   var temperature = data.list[0].main.temp
-  temperature = (((temperature-273.15)*9)/5)+32
+  // temperature = (((temperature-273.15)*9)/5)+32
     temperature = temperature.toFixed(1)
   var weatherDescription = data.list[0].weather[0].description
   var windSpeed = data.list[0].wind.speed
@@ -125,15 +132,18 @@ function currentForecast() {
   timerPage5.text(dayFive)
   
   console.log(data)
-
-  for(let i=0;i<40;i++) {
+    
+  for(let i=0;i<8;i++) {
+    
+   
     var timeText = data.list[i].dt_txt
     timeText = timeText.slice(11)
 
-      if (timeText=="12:00:00" && checkDay===0) {
-          checkDay=checkDay+1
+
+      if (timeText==="15:00:00") {
+          
           var temperature1 = data.list[i].main.temp
-          temperature1 = (((temperature1-273.15)*9)/5)+32
+          // temperature1 = (((temperature1-273.15)*9)/5)+32
           temperature1 = temperature1.toFixed(1)
           var weatherDescription1 = data.list[i].weather[0].description
   var windSpeed1 = data.list[i].wind.speed
@@ -149,19 +159,17 @@ function currentForecast() {
           $("#speed1").text("Wind Speed:" + windSpeed1 + " mph")
           $("#description1").text("Description:" + weatherDescription1)
           $("#humidity1").text("Humidity:" + humidity1 + "%")
-      }
-
-      if (timeText=="12:00:00" && checkDay===1) {
-        checkDay=checkDay+1
-        var temperature2 = data.list[i].main.temp
-        temperature2 = (((temperature2-273.15)*9)/5)+32
+          
+    
+          
+        var temperature2 = data.list[i+8].main.temp
         temperature2 = temperature2.toFixed(1)
-        var weatherDescription2 = data.list[i].weather[0].description
-var windSpeed2 = data.list[i].wind.speed
+        var weatherDescription2 = data.list[i+8].weather[0].description
+var windSpeed2 = data.list[i+8].wind.speed
   windSpeed2 = windSpeed2.toFixed(1)
-var humidity2 = data.list[i].main.humidity
+var humidity2 = data.list[i+8].main.humidity
 
-var imgIcon2 = data.list[i].weather[0].icon
+var imgIcon2 = data.list[i+16].weather[0].icon
     imgURL2 = 'http://openweathermap.org/img/w/'+imgIcon2+'.png'
   
   imgWeather2.attr('src',imgURL2)
@@ -170,10 +178,65 @@ var imgIcon2 = data.list[i].weather[0].icon
         $("#speed2").text("Wind Speed:" + windSpeed2 + " mph")
         $("#description2").text("Description:" + weatherDescription2)
         $("#humidity2").text("Humidity:" + humidity2 + "%")
+   
+
+        var temperature3 = data.list[i+16].main.temp
+        temperature3 = temperature3.toFixed(1)
+        var weatherDescription3 = data.list[i+16].weather[0].description
+var windSpeed3 = data.list[i+16].wind.speed
+  windSpeed3 = windSpeed3.toFixed(1)
+var humidity3 = data.list[i+16].main.humidity
+
+var imgIcon3 = data.list[i+16].weather[0].icon
+    imgURL3 = 'http://openweathermap.org/img/w/'+imgIcon3+'.png'
+  
+  imgWeather3.attr('src',imgURL3)
+
+        $("#temp3").text("Temp:" + temperature3 + " degrees")
+        $("#speed3").text("Wind Speed:" + windSpeed3 + " mph")
+        $("#description3").text("Description:" + weatherDescription3)
+        $("#humidity3").text("Humidity:" + humidity3 + "%")
+
+        var temperature4 = data.list[i+24].main.temp
+        temperature4 = temperature4.toFixed(1)
+        var weatherDescription4 = data.list[i+24].weather[0].description
+var windSpeed4 = data.list[i+24].wind.speed
+  windSpeed4 = windSpeed4.toFixed(1)
+var humidity4 = data.list[i+24].main.humidity
+
+var imgIcon4 = data.list[i+24].weather[0].icon
+    imgURL4 = 'http://openweathermap.org/img/w/'+imgIcon4+'.png'
+  
+  imgWeather4.attr('src',imgURL4)
+
+        $("#temp4").text("Temp:" + temperature4 + " degrees")
+        $("#speed4").text("Wind Speed:" + windSpeed4 + " mph")
+        $("#description4").text("Description:" + weatherDescription4)
+        $("#humidity4").text("Humidity:" + humidity4 + "%")
+
+        var temperature5 = data.list[i+32].main.temp
+        temperature5 = temperature5.toFixed(1)
+        var weatherDescription5 = data.list[i+32].weather[0].description
+var windSpeed5 = data.list[i+32].wind.speed
+  windSpeed5 = windSpeed5.toFixed(1)
+var humidity5 = data.list[i+32].main.humidity
+
+var imgIcon5 = data.list[i+32].weather[0].icon
+    imgURL5 = 'http://openweathermap.org/img/w/'+imgIcon5+'.png'
+  
+  imgWeather5.attr('src',imgURL5)
+
+        $("#temp5").text("Temp:" + temperature5 + " degrees")
+        $("#speed5").text("Wind Speed:" + windSpeed5 + " mph")
+        $("#description5").text("Description:" + weatherDescription5)
+        $("#humidity5").text("Humidity:" + humidity5 + "%")
+    
+
+
     }
 
 
-
-    }
-  })
+  }
+    
+})
 }
